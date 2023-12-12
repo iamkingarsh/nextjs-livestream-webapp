6f11e748deba14c74feaa480a3615ebd012ddcf7
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-13 02:04:47 +0530

    Update logo in navbar

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -13,9 +13,9 @@ export const Logo = () => {
   return (
     <Link href="/">
       <div className="flex items-center gap-x-4 hover:opacity-75 transition">
-        <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
+        <div className=" rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
           <Image
-            src="/spooky.svg"
+            src="https://gromer.web.app/Images/LandingPage/SVG/Symbol.svg"
             alt="Gamehub"
             height="32"
             width="32"
@@ -26,10 +26,10 @@ export const Logo = () => {
           font.className
         )}>
           <p className="text-lg font-semibold">
-            Gamehub
+            Gromer Streams
           </p>
           <p className="text-xs text-muted-foreground">
-            Let&apos;s play
+            Let&apos;s Game
           </p>
         </div>
       </div>
